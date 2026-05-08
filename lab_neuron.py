8e0ccf6439ef7300d27b1d938b40c508d6f0702a
ahanagemini <ahanagemini@gmail.com>
2026-05-08 13:00:38 -0400

    completing the lab

--- lab_neuron.py
+++ lab_neuron.py
@@ -3,10 +3,10 @@ import numpy as np
 
 def calculate_neuron_output(csv_file):
     # 1. TODO: Load the data from csv_file into a pandas DataFrame
-    df = None 
+    df = pd.read_csv(csv_file)
     
     # 2. TODO: Convert the DataFrame to a NumPy array (X)
-    X = None 
+    X = df.to_numpy()
     
     # 3. Define Weights (W) and Bias (b)
     W = np.array([0.5, -0.2])
@@ -14,9 +14,10 @@ def calculate_neuron_output(csv_file):
     
     # 4. TODO: Calculate z = XW + b 
     # Hint: Use the  .dot() method for multiplication
-    z = None 
+    z = (X.dot(W)) + b
     
     # 5. TODO: Apply Step Activation
     # Return 1 if z > 0, else 0
+    z = (z > 0).astype(int)
     
     return X, z
